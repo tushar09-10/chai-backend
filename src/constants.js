e293fe274d1839812065c12c2aca08c238bb5548
Tushar Singh <tushar09102003@gmail.com>
2025-08-16 23:30:23 +0530

    connect to mongo atlas

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = "videotube"
+export const MONGODB_URI = process.env.MONGODB_URI;
---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+
+const connectDB = async() => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`); 
+    }
+    catch(error){
+        console.error("MONGODB connection error: ", error);
+        // throw error;
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+export default connectDB;
